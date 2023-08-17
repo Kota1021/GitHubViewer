b68798412cec4a7f568c792e0a2c8f50cd320868
Kota1021 <Kota1021@users.noreply.github.com>
2023-08-17 23:22:55 +0900

    renamed "obj" into "object"

--- iOSEngineerCodeCheck/ViewController.swift
+++ iOSEngineerCodeCheck/ViewController.swift
@@ -41,8 +41,8 @@ class ViewController: UITableViewController, UISearchBarDelegate {
         if word.count != 0 {
             url = "https://api.github.com/search/repositories?q=\(word!)"
             task = URLSession.shared.dataTask(with: URL(string: url)!) { data, _, _ in
-                if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
-                    if let items = obj["items"] as? [[String: Any]] {
+                if let object = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
+                    if let items = object["items"] as? [[String: Any]] {
                         self.repositories = items
                         DispatchQueue.main.async {
                             self.tableView.reloadData()
